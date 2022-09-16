52f8eb1c9f3df0bd53f4335386e72eb7f376b5fe
Roberto Peixinho <rhpeixinho@gmail.com>
2022-09-16 13:25:19 -0300

    ToDo List Finalizada

--- aula-67/assets/js/script.js
+++ aula-67/assets/js/script.js
@@ -2,44 +2,95 @@ const addButton = document.querySelector('.container button');
 const toDoInput = document.querySelector('.container input');
 const wrapper = document.querySelector('.wrapper');
 
+// Criar Task clicando no botão
 addButton.addEventListener('click', (e) => {
     e.preventDefault();
-    let msg = toDoInput.value;
 
-    if (!msg) {
+    if (!toDoInput.value) {
         alert('Preencha o Campo Corretamente!');
         return;
     }
 
-    let task = createTask(msg);
-    wrapper.appendChild(task);
+    wrapper.appendChild(createTask(toDoInput.value));
+    clearInput();
+})
 
+// Criar Task apertando enter
+toDoInput.addEventListener('keypress', (e) => {
+
+    if (e.keyCode === 13) {
+
+        if (!toDoInput.value) {
+            alert('Preencha o Campo Corretamente!');
+            return;
+        }
+
+        wrapper.appendChild(createTask(toDoInput.value));
+        clearInput();
+    }
 
 })
+
+// // Deletar Task
+document.addEventListener('click', (e) => deleteTask(e.target))
+
+function clearInput(){
+    toDoInput.value = '';
+    toDoInput.focus();
+}
 
 function createTask (msg){
     const taskWrapper = document.createElement('article');
     taskWrapper.classList.add('task-container');
     taskWrapper.classList.add('neuromorphic-shadows');
+
+    const checkbox = createCheckbox();
+
+    const task = createTaskText(msg);
+
+    const deleteButton = createDeleteButton();
     
-    const checkmark = document.createElement('span');
-    checkmark.classList.add("checkmark");
-    checkmark.classList.add("neuromorphic-shadows-inside");
-
-    const done = document.createElement('input');
-    done.type = 'checkbox';
-
-    const task = document.createElement('p');
-    task.innerHTML = msg;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('neuromorphic-shadows')
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
-    
-    taskWrapper.appendChild(done);
-    taskWrapper.appendChild(checkmark);
+    taskWrapper.appendChild(checkbox);
     taskWrapper.appendChild(task);
     taskWrapper.appendChild(deleteButton);
     return taskWrapper;
+}
+
+function createCheckbox() {
+    const checkbox = document.createElement('div')
+    checkbox.classList.add('task-checkbox')
+
+    const done = document.createElement('input');
+    done.type = 'checkbox';
+    done.value = '1';
+    done.id = "checkbox-input";
+
+    const label = document.createElement('label');
+    label.htmlFor = "checkbox-input";
+    label.classList.add('neuromorphic-shadows-inside');
+
+    checkbox.appendChild(done);
+    checkbox.appendChild(label);
+
+    return checkbox;
+}
+
+function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('neuromorphic-shadows');
+    deleteButton.classList.add('delete');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+
+    return deleteButton;
+}
+
+function createTaskText(msg) {
+    const task = document.createElement('p');
+    task.innerHTML = msg;
+    return task;
+}
+
+function deleteTask(button) {
+    if (button.classList.contains('delete')) button.parentElement.remove();
 }
 

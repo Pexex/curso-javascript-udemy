a008c8987bb3f1da6a230ec6808696c31967b3ae
Roberto Peixinho <rhpeixinho@gmail.com>
2022-09-27 01:15:53 -0300

    Adicionada Pagina Inicial

---
+++ script.js
@@ -0,0 +1,46 @@
+const btns = document.querySelectorAll('.container button');
+
+btns[0].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-44/index.html";
+});
+
+btns[1].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-48/index.html";
+});
+
+btns[2].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-53/index.html";
+});
+
+btns[3].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-57/index.html";
+});
+
+btns[4].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-60/index.html";
+});
+
+btns[5].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-61/index.html";
+});
+
+btns[6].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-62/index.html";
+});
+
+btns[7].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-66/index.html";
+});
+
+btns[8].addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href="./Aula-67/index.html";
+});
